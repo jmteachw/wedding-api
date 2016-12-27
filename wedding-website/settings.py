6d8ec925e02b7ec51fe4f03bc7832fe1406c3848
Jordan Teachworth <jmteachw@gmail.com>
2016-12-27 03:04:08 -0600

    Added initial site template

--- wedding-website/settings.py
+++ wedding-website/settings.py
@@ -57,10 +57,18 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'wedding-website.urls'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            STATIC_ROOT + '/main'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,10 +126,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_URL = '/static/'
