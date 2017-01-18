02b69c1c0bf552b51d110ecf23e28b8426f1f1e2
Jordan Teachworth <jmteachw@gmail.com>
2017-01-18 00:34:00 -0600

    Made RSVP serializer a ModelSerializer to enforce the composite key relationship. Also remove old runserver script

--- api/serializers.py
+++ api/serializers.py
@@ -3,24 +3,8 @@ from rest_framework import serializers
 from api.models import RSVP
 
 
-class RSVPSerializer(serializers.Serializer):
+class RSVPSerializer(serializers.ModelSerializer):
 
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    email = serializers.EmailField(required=False, allow_blank=True)
-    attending = serializers.BooleanField(required=True)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `RSVP` instance, given the validated data.
-        """
-        return RSVP.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `RSVP` instance, given the validated data.
-        """
-        instance.email = validated_data.get('email', instance.email)
-        instance.attending = validated_data.get('attending', instance.attending)
-        instance.save()
-        return instance
+    class Meta:
+        model = RSVP
+        fields = ('first_name', 'last_name', 'email', 'attending')
